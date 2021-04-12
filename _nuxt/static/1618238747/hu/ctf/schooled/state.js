3e6d52879e35c5d14aaba52f3d93eee1ff4da9f0
Gergely Tarkó <hergo97@gmail.com>
2021-04-12 16:47:33 +0200

    Fixed bugs with language change

---
+++ _nuxt/static/1618238747/hu/ctf/schooled/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618238747",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhu\u002Fctf\u002Fschooled\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"800f2889"}}}}(null));
